49655c10ae4b5ea087660129364c08d5602b7a7e
dat225 <dat225@lehigh.edu>
2023-06-12 16:46:38 -0700

    updated styles for the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,28 @@
 import React from "react";
+import {createUseStyles} from 'react-jss';
+
+const useStyles = createUseStyles({
+    myBox: {
+        textAlign: 'center',
+        backgroundColor: '#9eebce',
+        display: 'inline-block',
+        borderRadius: '0.5rem',
+        padding: '1rem',
+        margin: '0.5rem',
+        borderWidth: '0.25rem',
+        boxShadow: '0 0.5rem 1rem rgba(0, 0, 0, 0.2)',
+        transition: 'transform 0.2s, box-shadow 0.2s',
+        '&:hover': {
+          transform: 'scale(1.05)',
+          boxShadow: '0 1rem 2rem rgba(0, 0, 0, 0.4)',
+        },
+      },
+})
 
 const Card = ({ name, email, id}) => {
+    const classes = useStyles()
     return (
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className={classes.myBox}>
             <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
             <div>
                 <h2> {name} </h2>
