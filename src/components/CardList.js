7cf3eb661a77f1ffa84bdfd0bb621f66a2ee049c
dat225 <dat225@lehigh.edu>
2023-06-13 12:39:38 -0700

    adding only first three robots

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,20 +2,21 @@ import React from "react";
 import Card from './Card';
 
 const CardList = ({robots}) => {
+    const someRobots = robots.slice(0, 3);
     return (
         <div>
             {
-            robots.map((user, i) => {
-                return (
+                someRobots.map((robot) => {
+                    return (
                     <Card 
-                        key={i} 
-                        id={robots[i].id} 
-                        name={robots[i].name} 
-                        email={robots[i].email}
-                    />
+                        key={robot.id} 
+                        id={robot.id} 
+                        name={robot.name} 
+                        email={robot.email}
+                            />
                     );
-            })
-        }
+                })
+            }
         </div>
     );
 }
