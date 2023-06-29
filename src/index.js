2ade9d49ecb5d7e5edb9226f8c9652356b30d26b
dat225 <dat225@lehigh.edu>
2023-06-29 12:51:49 -0700

    structure

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <RobotProvider>
-      <App />
+      <RobotProvider>
+        <App />
       </RobotProvider>
     </BrowserRouter>
   </React.StrictMode>
